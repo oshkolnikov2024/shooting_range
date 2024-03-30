c6f55dd378a374720495be8f35d7cffbf2a67b77
oshkolnikov2024 <npcprom@mail.ru>
2024-03-30 15:50:24 +0500

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,17 @@ SCRIEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCRIEN_HEIGHT))  #создаем переменную экран
 
 pygame.display.set_caption("Игра тир")  #придумываем название окна
-icon = pygame.image.load("")  # добавляем иконку 
+icon = pygame.image.load("img/icon.jpg")  # добавляем иконку
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")  # добавляем картинку цели
+target_width = 80   #размеры цели
+target_height = 80
+
+target_x = random.randint (0,SCREEN_WIDTH-target_width)    #hfyljvyst координаты x,y для цели
+target_y = random.randint (0,SCRIEN_HEIGHT-target_height)
+
+color = (random.randint(0,255), random.randint(0,255),random.randint(0,255))
 
 running = true
 
