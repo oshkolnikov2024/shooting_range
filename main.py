a91269e8f8bd2e27e0e6686c43811ca9f77e5926
oshkolnikov2024 <npcprom@mail.ru>
2024-03-30 15:16:00 +0500

    start winows

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800   #создаем переменные ширины и высоты окна
+SCRIEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCRIEN_HEIGHT))  #создаем переменную экран
+
+pygame.display.set_caption("Игра тир")  #придумываем название окна
+icon = pygame.image.load("")  # добавляем иконку 
+
 running = true
 
 while running:
